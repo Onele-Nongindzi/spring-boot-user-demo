38953733ce3cc7eea0c3dac46fb0634d9d7cb6db
Onele Nongindzi <onelenongindzi@gmail.com>
2025-05-16 10:18:08 +0200

    add FakeRepoInterface

---
+++ src/main/java/com/example/demo/repo/FakeRepoInterface.java
@@ -0,0 +1,9 @@
+package com.example.demo.repo;
+
+public interface FakeRepoInterface {
+    String insertUser(long id, String name, String surname);
+
+    String findUserById(long id);
+
+    String deleteUser(long id);
+}
